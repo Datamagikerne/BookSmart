e02de4b06741ca8ad0b50865afba63f152ab269c
Datamagikeren <lasse4040@gmail.com>
2022-11-25 13:03:18 +0100

    Add GetSubjects

--- BookSmart/Pages/Subjects/GetSubjects.cshtml.cs
+++ BookSmart/Pages/Subjects/GetSubjects.cshtml.cs
@@ -1,12 +1,25 @@
+using BookSmart.Models;
+using BookSmart.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using BookSmart.Models;
+using BookSmart.Services.Interfaces;
 namespace BookSmart.Pages.Subjects
 {
     public class GetSubjectsModel : PageModel
     {
+        public IEnumerable<Subject> Subjects { get; set; }
+
+
+
+        private ISubjectService context;
+        public GetSubjectsModel(ISubjectService service)
+        {
+            context = service;
+        }
         public void OnGet()
         {
+            Subjects = context.GetSubjects();
         }
     }
 }
--- BookSmart/Services/EFServices/EFSubjectService.cs
+++ BookSmart/Services/EFServices/EFSubjectService.cs
@@ -1,14 +1,22 @@
 ﻿using BookSmart.Models;
+using BookSmart.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookSmart.Services.EFServices
 {
-    public class EFSubjectService
+    public class EFSubjectService : ISubjectService
     {
         BookSmartDBContext context;
 
         public EFSubjectService(BookSmartDBContext context)
         {
             this.context = context;
+        }
+
+        public IEnumerable<Subject> GetSubjects()
+        {
+            return context.Subjects;
+
         }
     }
 }
