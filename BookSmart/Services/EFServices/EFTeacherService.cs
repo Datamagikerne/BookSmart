6939583ad0dfdf0a724a55e311fd7744102c6b7d
Anders Peter Jørgensen <ande9001@edu.easj.dk>
2022-11-28 13:27:57 +0100

    Add GetTeacher

--- BookSmart/Services/EFServices/EFTeacherService.cs
+++ BookSmart/Services/EFServices/EFTeacherService.cs
@@ -1,6 +1,43 @@
-﻿namespace BookSmart.Services.EFServices
+﻿using BookSmart.Models;
+using BookSmart.Services.Interfaces;
+
+
+namespace BookSmart.Services.EFServices
 {
-    public class EFTeacherService
+    public class EFTeacherService : ITeacherService
     {
+        BookSmartDBContext context;
+
+        public EFTeacherService(BookSmartDBContext context)
+        {
+            this.context = context;
+        }
+
+        public void CreateTeacher(Teacher Teacher)
+        {
+            context.Teachers.Add(Teacher);
+            context.SaveChanges();
+        }
+
+        public void DeleteTeacher(Teacher Teacher)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Teacher GetTeacher(int id)
+        {
+            return context.Teachers.Find(id);
+
+        }
+
+        public IEnumerable<Teacher> GetTeachers()
+        {
+            return context.Teachers;
+        }
+
+        public void UpdateTeacher(Teacher Teacher)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
