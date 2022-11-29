e7c25454180e9aec7d2479afaa86f2e830d49967
Ann-Sofie Jørgensen <annsofiej95@hotmail.com>
2022-11-29 12:09:13 +0100

    Added GetClasses

--- BookSmart/Services/EFServices/EFClassService.cs
+++ BookSmart/Services/EFServices/EFClassService.cs
@@ -1,6 +1,43 @@
-﻿namespace BookSmart.Services.EFServices
+﻿using BookSmart.Models;
+using BookSmart.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BookSmart.Services.EFServices
 {
-    public class EFClassService
+    public class EFClassService : IClassService
     {
+        BookSmartDBContext context;
+
+        public EFClassService(BookSmartDBContext context)
+        {
+            this.context = context;
+        }
+
+        public void CreateClass(Class Class)
+        {
+            context.Classes.Add(Class);
+            context.SaveChanges();
+        }
+
+        public Class GetClass(string name)
+        {
+            return context.Classes.Find(name);
+        }
+
+        public void UpdateClass(Class Class)
+        {
+            Class c = GetClass(Class.ClassName);
+            context.Entry(c).CurrentValues.SetValues(class);
+            context.SaveChanges();
+        }
+
+        public IEnumerable<Class> GetClasses()
+        {
+            return context.Classes;
+        }
     }
 }
+
+   
+
