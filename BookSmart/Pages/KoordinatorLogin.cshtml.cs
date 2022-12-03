bb14b72d9807be2806ce38f1e9d7b86be3358548
Siluu <silk0293@edu.zealand.dk>
2022-12-03 10:41:45 +0100

    .

---
+++ BookSmart/Pages/KoordinatorLogin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookSmart.Pages
+{
+    public class KoordinatorLoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
