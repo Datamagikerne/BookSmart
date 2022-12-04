92e6c13421f6aa239ab44871d307399bc81aca59
Datamagikeren <lasse4040@gmail.com>
2022-12-04 17:18:04 +0100

    Add koordinator menu

---
+++ BookSmart/Pages/KoordinatorMenu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookSmart.Pages
+{
+    public class KoordinatorMenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
