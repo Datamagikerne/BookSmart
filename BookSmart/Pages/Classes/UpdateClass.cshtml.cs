9d760afc35d4706be035e6c72d481f394988474d
Siluu <silk0293@edu.zealand.dk>
2022-12-06 13:55:06 +0100

    ,,

--- BookSmart/Pages/Classes/UpdateClass.cshtml.cs
+++ BookSmart/Pages/Classes/UpdateClass.cshtml.cs
@@ -5,51 +5,58 @@ using BookSmart.Services;
 using BookSmart.Services.Interfaces;
 using BookSmart.Services.Interfaces.CorrelationTables;
 using BookSmart.Services.EFServices;
+using BookSmart.Pages.Classes;
 
 namespace BookSmart.Pages.Classes
 {
-    public class UpdateClassModel : PageModel
-    {
-        IClassService classService;
-        IBookClassService bcService;
-        IBookService bookService;
-        IClassTeacherService ctService;
-        ITeacherService teacherService;
-
-        public UpdateClassModel(IClassService classService, IBookClassService bcService, IBookService bookService, IClassTeacherService ctService, ITeacherService teacherService)
+        public class UpdateClassModel : PageModel
         {
-            this.classService = classService;
-            this.bcService = bcService;
-            this.bookService = bookService;
-            this.ctService = ctService;
-            this.teacherService = teacherService;
-        }
+            IClassService classService;
+            IBookClassService bcService;
+            IBookService bookService;
+            IClassTeacherService ctService;
+            ITeacherService teacherService;
 
-        [BindProperty]
-        public Class Class { get; set; }
+            public UpdateClassModel(IClassService classService, IBookClassService bcService, IBookService bookService, IClassTeacherService ctService, ITeacherService teacherService)
+            {
+                this.classService = classService;
+                this.bcService = bcService;
+                this.bookService = bookService;
+                this.ctService = ctService;
+                this.teacherService = teacherService;
+            }
 
-        #region BookClass checkbox
-        [BindProperty]
-        public List<int> ChosenBooksIds { get; set; }
-        public IEnumerable<Book> Books { get; set; }
-        public BookClass BookClass { get; set; }
-        public int Checker { get; set; }
-        #endregion
+            [BindProperty]
+            public Teacher Teacher { get; set; }
+            [BindProperty]
+            public Book Book { get; set; }
+            [BindProperty]
+            public Class Class { get; set; }
+            
+            #region BookClass checkbox
+            [BindProperty]
+            public List<int> ChosenBooksIds { get; set; }
+            public IEnumerable<Book> Books { get; set; }
+            public IEnumerable<Class> Classes { get; set; }
+            public BookClass BookClass { get; set; }
+            public int Checker { get; set; }
+            #endregion
 
-        #region ClassTeacher checkbox
-        [BindProperty]
-        public List<string> ChosenTeacherIds { get; set; }
-        public IEnumerable<Teacher> Teachers { get; set; }
-        public ClassTeacher ClassTeacher { get; set; }
-        public int Checker2 { get; set; }
-        #endregion
+            #region ClassTeacher checkbox
+            [BindProperty]
+            public List<string> ChosenTeacherIds { get; set; }
+            public IEnumerable<Teacher> Teachers { get; set; }
+            public ClassTeacher ClassTeacher { get; set; }
+            public int Checker2 { get; set; }
+            #endregion
 
-        public void OnGet(int cid)
-        {
-            Class = classService.GetClass(cid);
-            Books = bookService.GetBooks();
-            Teachers = teacherService.GetTeachers();
-        }
+            public void OnGet(int cid)
+            {
+                Class = classService.GetClass(cid);
+                Books = bookService.GetBooks();
+                Classes = classService.GetClasses();
+                Teachers = teacherService.GetTeachers();
+            }
 
         public IActionResult OnPost()
         {
