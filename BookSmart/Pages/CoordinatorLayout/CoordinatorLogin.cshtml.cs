c9145874dfb79d63d6a23c395437ef366e277363
Siluu <silk0293@edu.zealand.dk>
2022-12-12 11:15:15 +0100

    Starten på koordinator menu

--- BookSmart/Pages/CoordinatorLayout/CoordinatorLogin.cshtml.cs
+++ BookSmart/Pages/CoordinatorLayout/CoordinatorLogin.cshtml.cs
@@ -5,23 +5,6 @@ namespace BookSmart.Pages.CoordinatorLayout
 {
     public class CoordinatorLoginModel : PageModel
     {
-        private ITemaRepository repo;
-        public BrugermenuModel(ITemaRepository repository)
-        {
-            repo = repository;
-        }
-        public List<Tema> Temaer { get; private set; }
-        [BindProperty]
-        public Tema tema { get; set; }
-        public IActionResult OnGet()
-        {
-            tema = new Tema();
-            Temaer = repo.GetAllTema();
-            return Page();
-        }
-        public void OnPost()
-        {
-
-        }
+       
     }
 }
