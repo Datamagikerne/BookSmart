18339f29cd53187030b41fd9b9ac3a3ef1616eae
Datamagikeren <lasse4040@gmail.com>
2022-12-13 12:21:39 +0100

    Add design og nye layouts

---
+++ BookSmart/Pages/Shared/TeacherLayoutModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookSmart.Pages.Shared
+{
+    public class TeacherLayoutModel
+    {
+        [BindProperty]
+        public string Test { get; set; }
+
+
+
+    }
+}
